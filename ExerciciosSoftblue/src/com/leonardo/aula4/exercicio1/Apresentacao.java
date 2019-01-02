500ee019680b5cb66d7ab2f07153740dadeb9480
LeonardJMelo <leonardojm51@hotmail.com>
2019-01-02 20:08:38 -0200

    Modificado para receber entrada de dados para definir as horas

--- ExerciciosSoftblue/src/com/leonardo/aula4/exercicio1/Apresentacao.java
+++ ExerciciosSoftblue/src/com/leonardo/aula4/exercicio1/Apresentacao.java
@@ -1,5 +1,7 @@
 package com.leonardo.aula4.exercicio1;
 
+import java.util.Scanner;
+
 /*
  * @author Leonardo Melo
  * Objetivo: Ajustar a respectiva hora, baseando-se na posição do ponteiro no relógio.
@@ -7,9 +9,17 @@ package com.leonardo.aula4.exercicio1;
  */
 public class Apresentacao {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		Relogio relogio = new Relogio();
 		try {
-			relogio.acertarRelogio(17, 12, 12);
+			System.out.print("Informe a hora desejada: ");
+			int hora = scan.nextInt();
+			System.out.print("Informe a posição do pinteiro para minutos: ");
+			int minuto = scan.nextInt();
+			System.out.print("Informe a posição do ponteiro para segundos: ");
+			int segundo = scan.nextInt();
+			scan.close();
+			relogio.acertarRelogio(hora, minuto, segundo);
 			System.out.printf("%02d:%02d:%02d",relogio.getPonteiroHora(),relogio.getPonteiroMinuto(),relogio.getPonteiroSegundo());
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
