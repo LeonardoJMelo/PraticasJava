e9fbd4e2d243c9aadee708ef4db17363b929d760
LeonardJMelo <leonardojm51@hotmail.com>
2019-01-11 14:15:57 -0200

    Refatoracao feita.

--- ExerciciosSoftblue/src/com/leonardo/aula9/exercicio3/Apresentacao.java
+++ ExerciciosSoftblue/src/com/leonardo/aula9/exercicio3/Apresentacao.java
@@ -1,8 +1,9 @@
+package com.leonardo.aula9.exercicio3;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Period;
-
 /*
  * @author Leo Melo
  * Objetivo: Usar a API time do java para levantar algumas informações.
@@ -14,16 +15,16 @@ public class Apresentacao {
 		calculaSegundosAteOMomento();
 	}
 	public static void calculaSegundosAteOMomento() {
-		LocalTime dataInicial = LocalTime.parse("03:00:00");
-		LocalTime dataFinal = LocalTime.now();
+		LocalTime horaInicial = LocalTime.parse("03:00:00");
+		LocalTime horaFinal = LocalTime.now();
 		
-		Duration d = Duration.between(dataInicial, dataFinal);
-		System.out.println("Se passaram "+d.getSeconds()+" segundos, das 3:00 AM até o momento.");
+		Duration duracao = Duration.between(horaInicial, horaFinal);
+		System.out.println("Se passaram "+duracao.getSeconds()+" segundos, das 3:00 AM até o momento.");
 	}
 	public static void calcularIdadeJamesGosling() {
 		LocalDate dataAtual = LocalDate.now();
 		LocalDate dataAniversario = LocalDate.parse("1955-05-19");
-		Period p = Period.between(dataAniversario,dataAtual);
-		System.out.println("A idade de James Gosling é: "+p.getYears()+" anos.");
+		Period periodo = Period.between(dataAniversario,dataAtual);
+		System.out.println("A idade de James Gosling é: "+periodo.getYears()+" anos.");
 	}
 }
